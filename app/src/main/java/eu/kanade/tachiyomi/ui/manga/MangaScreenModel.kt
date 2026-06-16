a96f994d1dd7ed7c99f10832185b9496663b33ec
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2026-06-16 19:58:38 +0600

    Remove redundant call to `updateMangaFromRemote` in manga screen
    (cherry picked from commit 082bd66673a65215e0c8f750c56f1958ced6243b)

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaScreenModel.kt
@@ -286,7 +286,7 @@ class MangaScreenModel(
             observeTrackers()
 
             // Fetch info-chapters when needed
-            if (screenModelScope.isActive) {
+            if ((needRefreshInfo || needRefreshChapter) && screenModelScope.isActive) {
                 fetchAllFromSource(
                     manualFetch = false,
                     fetchDetails = needRefreshInfo,
